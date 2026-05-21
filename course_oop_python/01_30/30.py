ae237c165172d2962740c1b393e7808f0bedd306
ersildan <11free11@mail.ru>
2026-05-21 19:28:09 +0300

    +

---
+++ course_oop_python/01_30/30.py
@@ -0,0 +1,21 @@
+class Cat:
+    def __init__(self, name):
+        self._name = name
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if isinstance(name, str) and name.isalpha():
+            self._name = name
+        else:
+            raise ValueError('Некорректное имя')
+
+    name = property(get_name, set_name)
+
+
+cat = Cat('Кемель')
+print(cat.name)
+cat.name = 'Роджер'
+
+print(cat.name)
